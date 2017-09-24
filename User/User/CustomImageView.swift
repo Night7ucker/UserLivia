32682d9b30660a27ad569b8be439dfe8f7360600
egoryanukovich <drummlover@tut.by>
2017-09-24 15:59:30 +0300

    Added custom image file to local repository

---
+++ User/User/CustomImageView.swift
@@ -0,0 +1,32 @@
+//
+//  CustomImageView.swift
+//  User
+//
+//  Created by Egor Yanukovich on 9/24/17.
+//  Copyright © 2017 BAMFAdmin. All rights reserved.
+//
+
+import UIKit
+
+class CustomImageView: UIImageView {
+    
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        self.setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    func setup()
+    {
+        //Do onetime setup of the shape layer.
+        layer.cornerRadius = frame.size.width/2
+        layer.masksToBounds = true
+    }
+
+}
