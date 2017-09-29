3b7d2175c7e879a27285150efaa70ec8aa205844
Stanislau <stanislau.sakharchuk.work@gmail.com>
2017-09-29 20:36:31 +0300

    Deleted comments.

--- User/ReminderRequests.swift
+++ User/ReminderRequests.swift
@@ -138,49 +138,6 @@ class ReminderRequests {
         }
     }
     
-    //    func getReminderForEditing(reminderId: String, completion: @escaping (Bool, ReminderModel) -> Void) {
-    //
-    //        let url = "https://test.liviaapp.com/api/reminder/" + reminderId
-    //
-    //        let headers = [
-    //            "Content-Type": "application/json",
-    //            "LiviaApp-Token": RealmDataManager.getTokensFromRealm()[0].accessToken!,
-    //            "LiviApp-country": RealmDataManager.getUserDataFromRealm()[0].countryCode!,
-    //            "LiviaApp-city": RealmDataManager.getUserDataFromRealm()[0].cityId!,
-    //            "LiviaApp-language": "en",
-    //            "LiviaApp-timezone": "180",
-    //            "LiviaApp-APIVersion": "2.0"
-    //        ]
-    //
-    //
-    //
-    //        Alamofire.request(url, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
-    //
-    //            guard let result = response.result.value as? [String : AnyObject] else{ return }
-    //            guard let body = result["body"] as? [[String : AnyObject]] else { return }
-    //
-    //            let reminderObject = ReminderModel()
-    //
-    //            for element in body {
-    //
-    //
-    //                reminderObject.id = element["id"] as? String
-    //                reminderObject.medicineName = element["drug_name"] as? String
-    //
-    //                let reminderCheckboxIndex = element["reminder"] as? String
-    //                reminderObject.checkBoxIndex = Int(reminderCheckboxIndex!)!
-    //
-    //                reminderObject.dateForRequest = element["start_date"] as? String
-    //
-    //                let date = self.formDateFromDateFromRequest(date: reminderObject.dateForRequest!)
-    //                reminderObject.dateTimeDaysAndYears = date.1
-    //                reminderObject.dateTimeHoursAndMinutes = date.0
-    //            }
-    //
-    //            completion(true, reminderObject)
-    //        }
-    //    }
-    
     func formDateFromDateFromRequest(date: String) -> (String, String) {
         var hourAndMinuteDate = String()
         var dayAndMonthDate = String()
@@ -195,67 +152,4 @@ class ReminderRequests {
         return (hourAndMinuteDate, dayAndMonthDate)
     }
 }
-
-//        Alamofire.request(url, method: .patch, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
-//
-//            guard let result = response.result.value as? [String : AnyObject] else{ return }
-//            guard let body = result["body"] as? [[String : AnyObject]] else { return }
-//
-//            for element in body {
-//                let country = element["country"] as? String
-//                let city = element["name"] as? String
-//
-//                let cityObject = City()
-//
-//                cityObject.cityName = city
-//                cityObject.countryName = country
-//
-//                let realm = try! Realm()
-//                RealmDataManager.writeIntoRealm(object: cityObject, realm: realm)
-//
-//            }
-//            completion(true)
-//        }
-
-//    static func getCitiesForSearchRequest(searchStringForCities: String, completion: @escaping (Bool) -> Void) {
-//        let headers = [
-//            "LiviaApp-language": "en",
-//            "LiviaApp-timezone": "180",
-//            "LiviaApp-APIVersion": "2.0"
-//        ]
-//
-//        let url = "https://test.liviaapp.com/api/city?active=1&offset=0&limit=20&search=\(searchStringForCities)"
-//
-//        let realm = try! Realm()
-//        if RealmDataManager.getCitiesNamesFromRealm().count != 0 {
-//            try! realm.write {
-//                realm.delete(RealmDataManager.getCitiesNamesFromRealm())
-//            }
-//        }
-//
-//        Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers).responseJSON { (response) in
-//
-//
-//
-//            guard let result = response.result.value as? [String : AnyObject] else{ return }
-//            print(result)
-//            guard let body = result["body"] as? [[String : AnyObject]] else { return }
-//
-//            for element in body {
-//                let country = element["country"] as? String
-//                let city = element["name"] as? String
-//
-//                let cityObject = City()
-//
-//                cityObject.cityName = city
-//                cityObject.countryName = country
-//
-//                let realm = try! Realm()
-//                RealmDataManager.writeIntoRealm(object: cityObject, realm: realm)
-//
-//            }
-//            completion(true)
-//        }
-//    }
-
 
