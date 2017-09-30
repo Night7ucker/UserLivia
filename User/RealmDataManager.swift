3a9f9df48b4590ef72b35140b0dd2fb1e1af3455
BAMFAdmin <BAMFAdmin@Mac-mini-MacMini.local>
2017-09-30 18:06:03 +0300

    Did realm static and delete realm announcement in RealmDataManager methods

--- User/RealmDataManager.swift
+++ User/RealmDataManager.swift
@@ -10,7 +10,7 @@ import Foundation
 import RealmSwift
 
 class RealmDataManager {
-    
+    static let realm = try! Realm()
     static func writeIntoRealm(object: Object) {
         let realm = try! Realm()
         try! realm.write {
@@ -19,48 +19,39 @@ class RealmDataManager {
     }
     
     static func getDataFromCountries() -> Results<CountryCodesModel> {
-        let realm = try! Realm()
         let data = realm.objects(CountryCodesModel.self)
         return data
     }
     static func getPhoneNumberFromRealm() -> Results<PhoneNumberModel> {
-        let realm = try! Realm()
         let data = realm.objects(PhoneNumberModel.self)
         return data
     }
     static func getTokensFromRealm() -> Results<TokensModel> {
-        let realm = try! Realm()
         let data = realm.objects(TokensModel.self)
         return data
     }
 
     static func getRemindersFromRealm() -> Results<ReminderModel> {
-        let realm = try! Realm()
         let data = realm.objects(ReminderModel.self)
         return data
     }
     static func getImageUrlFromRealm() -> Results<UploadImageModel> {
-        let realm = try! Realm()
         let data = realm.objects(UploadImageModel.self)
         return data
     }
     static func getUserDataFromRealm() -> Results<UserModel> {
-        let realm = try! Realm()
         let data = realm.objects(UserModel.self)
         return data
     }
     static func getCitiesNamesFromRealm() -> Results<City> {
-        let realm = try! Realm()
         let data = realm.objects(City.self)
         return data
     }
     static func getDrugsFromRealm() -> Results<GetDrugsModel> {
-        let realm = try! Realm()
         let data = realm.objects(GetDrugsModel.self)
         return data
     }
     static func getDrugsDescriptionFromRealm() -> Results<DrugsDescriptionModel> {
-        let realm = try! Realm()
         let data = realm.objects(DrugsDescriptionModel.self)
         return data
     }
