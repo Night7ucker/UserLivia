ae3367442659043311cc94cb2ef7bf854eb480e7
Stanislau <stanislau.sakharchuk.work@gmail.com>
2017-10-01 18:01:35 +0300

    Tested pod for collapsed sections.

--- User/User/TestViewController.swift
+++ User/User/TestViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CollapsibleTableSectionViewController
 
-class TestViewController: UITableViewController {
+class TestViewController: CollapsibleTableSectionViewController {
 
     @IBOutlet weak var testTableViewOutlet: UITableView!
     
@@ -22,9 +22,6 @@ class TestViewController: UITableViewController {
     
         testTableViewOutlet.delegate = self
         testTableViewOutlet.dataSource = self
-        
-        tableView.estimatedRowHeight = 44.0
-        tableView.rowHeight = UITableViewAutomaticDimension
         
 //        testTableViewOutlet.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height/2)
         
@@ -47,68 +44,61 @@ class TestViewController: UITableViewController {
     func buttonAction() {
         print("button tapped")
     }
-
 }
 
-extension TestViewController {
+extension TestViewController: CollapsibleTableSectionDelegate {
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
+    func numberOfSections(_ tableView: UITableView) -> Int {
+        return 2
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sections[section].collapsed ? 0 : sections[section].items.count
+    func collapsibleTableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
     
     // Cell
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = testTableViewOutlet.dequeueReusableCell(withIdentifier: "testCell") as! TestTableViewCell
+    func collapsibleTableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell") as! TestTableViewCell
         
-        cell.testLabelOutlet.text = "GOvno"
+        cell.textLabel?.text = "Cell Text"
         
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableViewAutomaticDimension
-    }
     
-    // Header
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as? CollapsibleTableViewHeader ?? CollapsibleTableViewHeader(reuseIdentifier: "header")
-        
-        header.titleLabel.text = sections[section].name
-        header.arrowLabel.text = ">"
-        header.setCollapsed(sections[section].collapsed)
-        
-        header.section = section
-        header.delegate = self
-        
-        return header
-    }
     
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 44.0
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 1.0
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func collapsibleTableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("Selected row section is \(indexPath.section), it's row is \(indexPath.row)")
+    }
+    
+    func shouldCollapseOthers(_ tableView: UITableView) -> Bool {
+        return true
     }
 }
 
 extension TestViewController: CollapsibleTableViewHeaderDelegate {
     
     func toggleSection(_ header: CollapsibleTableViewHeader, section: Int) {
-        let collapsed = !sections[section].collapsed
-        
-        sections[section].collapsed = collapsed
-        header.setCollapsed(collapsed)
-        
-        tableView.reloadSections(NSIndexSet(index: section) as IndexSet, with: .automatic)
+        if sections[section].collapsed == true {
+            switch section {
+            case 0:
+                sections[0].collapsed = false
+                sections[1].collapsed = true
+                testTableViewOutlet.reloadSections(NSIndexSet(index: 0) as IndexSet, with: .automatic)
+                testTableViewOutlet.reloadSections(NSIndexSet(index: 1) as IndexSet, with: .automatic)
+            case 1:
+                sections[1].collapsed = false
+                sections[0].collapsed = true
+                testTableViewOutlet.reloadSections(NSIndexSet(index: 0) as IndexSet, with: .automatic)
+                testTableViewOutlet.reloadSections(NSIndexSet(index: 1) as IndexSet, with: .automatic)
+            default:
+                break
+            }
+        }
+//        let collapsed = !sections[section].collapsed
+//        
+//        sections[section].collapsed = collapsed
+//        header.setCollapsed(collapsed)
     }
 }
 
@@ -138,9 +128,9 @@ public var sectionsData: [Section] = [
     Section(name: "Self-collect", items: [
         Item(name: "MacBook", detail: "Apple's ultraportable laptop, trading portability for speed and connectivity."),
         Item(name: "Accessories", detail: "")
-        ]),
+        ], collapsed: true),
     Section(name: "iPad", items: [
         Item(name: "iPad Pro", detail: "iPad Pro delivers epic power, in 12.9-inch and a new 10.5-inch size."),
         Item(name: "Accessories", detail: "")
-        ])
+        ], collapsed: false)
 ]
